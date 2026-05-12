8397fa9908a7742656b475d6006d47685bfc6947
suhilmi01 <suhilmi01@mail.com>
2026-05-12 17:07:31 +0700

    membuat class jantung

--- jantung.cpp
+++ jantung.cpp
@@ -4,3 +4,16 @@
 #include <iostream>
 using namespace std;
 
+class Jantung {
+public:
+    // membuat constructor dan destructor dari class jantung
+    Jantung() {
+        cout << "Jantung Ditambah\n";
+    }
+
+    ~Jantung() {
+        cout << "Jantung Dimatikan\n";
+    }
+};
+
+#endif
