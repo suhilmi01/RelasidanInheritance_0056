5d67c57f7a4c3144897565c7d107d64ec81e9471
suhilmi01 <suhilmi01@mail.com>
2026-05-12 17:02:24 +0700

    pembuatan class dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -25,3 +25,23 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    // deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    // pembuatan constructor dan destructor dari class dokter
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    // deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
