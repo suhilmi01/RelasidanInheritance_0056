1b9590a224e1dc63bb80ea8f0d5409d8e50b60d6
suhilmi01 <suhilmi01@mail.com>
2026-05-12 16:59:21 +0700

    membuat file header anak dan ibu

--- agregrasi.cpp
+++ agregrasi.cpp
@@ -7,23 +7,3 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main()
-{
-    //deklarasi dan pemberian nilai variabel pointer untuk objek dari class ibu dan anak
-    Ibu* varIbu = new Ibu("rani");
-    Ibu* varIbu2 = new Ibu("sari");
-    anak* varAnak1 = new anak("tono");
-    anak* varAnak2 = new anak("tiara");
-    anak* varAnak3 = new anak("dini");
-
-    //pemanggilan dan pemberian argument pada prosedur tambahAnak()
-    varIbu->tambahAnak(varAnak1);
-    varIbu->tambahAnak(varAnak2);
-    varIbu2->tambahAnak(varAnak3);
-
-    //pemanggilan prosedur cetakAnak()
-    varIbu->cetakAnak();
-    varIbu2->cetakAnak();
-
-    return 0;
-}
