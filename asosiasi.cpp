71e3cb7aeba6bbadfee18877436a9d8308a0d50b
suhilmi01 <suhilmi01@mail.com>
2026-05-12 17:02:00 +0700

    deklarasi class dokter dan pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,4 +3,25 @@
 #include <string>
 using namespace std;
 
+class dokter;
 
+// deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    // deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    // pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
+    // deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
