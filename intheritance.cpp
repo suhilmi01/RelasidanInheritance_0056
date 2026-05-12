ac055d9319fb77cdc6e921d4b7a9977af1e63e97
suhilmi01 <suhilmi01@mail.com>
2026-05-12 17:05:24 +0700

    membuat class orang

--- intheritance.cpp
+++ intheritance.cpp
@@ -3,3 +3,22 @@
 
 using namespace std;
 
+class orang {
+public:
+    // deklarasi member variabel dari class orang
+    string nama;
+
+    // pembuatan constructor dan destructor dari class orang
+    orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+    // pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
