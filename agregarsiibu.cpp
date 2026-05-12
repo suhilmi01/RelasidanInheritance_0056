42e84c8561ea9cd98d75e67a8733baf1ffda67fa
suhilmi01 <suhilmi01@mail.com>
2026-05-12 16:56:28 +0700

    membuat class ibu

--- agregarsiibu.cpp
+++ agregarsiibu.cpp
@@ -1,8 +1,18 @@
 #ifndef IBU_H
 #define IBU_H
-
-#include <iostream>
-#include <string>
 #include <vector>
-using namespace std;
 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
